7d1c2d9053ee38bbc2fe1013c4211e97524e3ad0
Gautierrr <gaut.vauloup@gmail.com>
2024-11-09 18:52:32 +0100

    For merge

--- .bat
+++
@@ -1,6 +0,0 @@
-gcc -o main main.c src/create_championship.c src/main_menu.c src/add_team.c src/team_balancing.c src/save_binary_tree.c src/display_binary_tree.c src/search_team.c src/delete_team.c src/edit_team.c
-
-
-
-
-:: comment  src/insert_team.c src/insert_exemple.c src/main_menu_exemple.c
